5bd8ac21ea279ebf901a103787f4131cdb17fbf7
Valentin Niess <niess@in2p3.fr>
2017-12-15 22:09:01 +0100

    Compute the rate for the Waxman Bahcall bound

--- scripts/show-events.py
+++ scripts/show-events.py
@@ -9,7 +9,9 @@ from grand_tour import Topography
 
 # Settings
 DATA_DIR = "share/events"
-
+PHI0 = 2E-08
+E0 = 10**7.5
+E1 = 10**14.5
 
 # Load and parse the events
 topo = Topography(43, 87, "flat/4")
@@ -33,7 +35,12 @@ for name in os.listdir(DATA_DIR):
         position.append(r)
         weight.append(w)
 position = numpy.array(position) * 1E-03
-weight = numpy.array(weight)
+year = 365.25 * 24. * 60. * 60.
+weight = numpy.array(weight) * PHI0 * (1. / E0 - 1. / E1) * year
+mu = sum(weight) / generated
+sigma = sum(weight**2) / generated
+sigma = numpy.sqrt((sigma - mu**2) / generated)
+print "Total rate = {:.3f} +-{:.3f} a^-1".format(mu, sigma)
 
 
 def plot_histogram(samples):
@@ -42,7 +49,7 @@ def plot_histogram(samples):
     n, x = numpy.histogram(samples, 40, weights=weight / generated)
     n2, _ = numpy.histogram(samples, x, weights=weight**2 / generated)
     x = 0.5 * (x[1:] + x[:-1])
-    norm = generated / (sum(weight) * (x[1] - x[0]))
+    norm = 1. / (x[1] - x[0])
     p = n * norm
     dp = numpy.sqrt((n2 - n * n) / generated) * norm
     plt.figure()
@@ -53,18 +60,20 @@ def plot_histogram(samples):
 plt.style.use("deps/mplstyle-l3/style/l3.mplstyle")
 
 plot_histogram(theta)
-plt.xlabel(r"$\theta$ (deg)")
-plt.ylabel(r"pdf (deg$^{-1}$)")
+plt.xlabel(r"$\theta_\tau$ (deg)")
+plt.ylabel(r"rate (deg$^{-1}$ a$^{-1}$)")
 
 plot_histogram(phi)
-plt.axis((-200., 200., 0., 6E-03))
-plt.xlabel(r"$\phi$ (deg)")
-plt.ylabel(r"pdf (deg$^{-1}$)")
+plt.axis((-200., 200., 0., 8E-04))
+plt.xticks(numpy.linspace(-180., 180., 5))
+plt.yticks(numpy.linspace(0., 8E-04, 5))
+plt.xlabel(r"$\phi_\tau$ (deg)")
+plt.ylabel(r"rate (deg$^{-1}$ a$^{-1}$)")
 
 plot_histogram(altitude)
-plt.axis((0., 1., 0., 3.))
+# plt.axis((0., 1., 0., 3.))
 plt.xlabel(r"decay altitude (km)")
-plt.ylabel(r"pdf (km$^{-1}$)")
+plt.ylabel(r"rate (km$^{-1}$ a$^{-1}$)")
 
 plt.figure()
 plt.plot(position[:, 0], position[:, 1], "k.")
