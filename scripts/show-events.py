07d2676dc3a050a9ee96a9320fc614a61870fa61
Valentin Niess <niess@in2p3.fr>
2017-12-19 13:56:39 +0100

    Split Earth skimming and atmospheric events in the plot

--- scripts/show-events.py
+++ scripts/show-events.py
@@ -31,22 +31,35 @@ def load(path):
             e, r, u = event["tau_at_decay"]
             t, p = topo.local_to_angular(r, u)
             _, _, a = topo.local_to_lla(r)
-            w = sum(v[0] for v in event["primaries"][0]) / event["primaries"][1]
-            w *= event["statistics"][0] * PHI0 * year
+            K = [numpy.linalg.norm(v[3]) > 6371E+03
+                 for v in event["primaries"][0]]
+            J = [not k for k in K]
+            wp = numpy.array([v[0] for v in event["primaries"][0]])
+            norm = event["statistics"][0] * PHI0 * year / event["primaries"][1]
+            wE = sum(wp[J]) * norm
+            wA = sum(wp[K]) * norm
             generated += event["statistics"][1]
-            if t < 90.: continue
+            if wE + wA == 0:
+                continue
             theta.append(t)
             phi.append(p)
             altitude.append(a * 1E-03)
             position.append(r)
             energy.append(e)
-            weight.append(w)
+            weight.append((wE, wA))
     position = numpy.array(position) * 1E-03
     weight = numpy.array(weight)
-    mu = sum(weight) / generated
-    sigma = sum(weight**2) / generated
-    sigma = numpy.sqrt((sigma - mu**2) / generated)
-    print "Total rate = {:.3f} +-{:.3f} a^-1".format(mu, sigma)
+    def get_stats(w, n):
+        mu = sum(w) / n
+        sigma = sum(w**2) / n
+        sigma = numpy.sqrt((sigma - mu**2) / n)
+        return mu, sigma
+    mE, sE = get_stats(weight[:,0], generated)
+    mA, sA = get_stats(weight[:,1], generated)
+    print "Earth skimming = {:.3f} +-{:.3f} a^-1".format(mE, sE)
+    print "Atmospheric    = {:.3f} +-{:.3f} a^-1".format(mA, sA)
+    print "Events ratio   = {:.2f}".format(
+        sum(weight[:,0] > 0.) / float(len(weight)))
 
     return theta, phi, altitude, position, energy, weight, generated
 
@@ -91,23 +104,27 @@ def plot_histogram(samples, weight, generated, plot=plt.plot, clr="k",
 # Show the distributions
 plt.style.use("deps/mplstyle-l3/style/l3.mplstyle")
 
-x, p = plot_histogram(energy, weight, generated, plot=plt.loglog, factor=1)
+x, p = plot_histogram(energy, weight[:, 0], generated, plot=plt.loglog,
+                      factor=1)
+x2, p2 = plot_histogram(energy, weight[:, 1], generated, plot=plt.loglog,
+                        factor=1, new=False, clr="r")
 plt.xlabel(r"energy, E$_\tau$ (GeV)")
 plt.ylabel(r"E$_\tau \times$ rate (a$^{-1}$)")
 plt.savefig("tau-energy.png")
 
 plt.figure()
-plt.semilogx(x, cumtrapz(p, x, initial=0.), "k-")
+plt.semilogx(x, cumtrapz(p, x, initial=0.) / numpy.trapz(p, x), "k-")
+plt.semilogx(x2, cumtrapz(p2, x2, initial=0.) / numpy.trapz(p2, x2), "r-")
 plt.xlabel(r"energy limit, E$_\tau$ (GeV)")
-plt.ylabel(r"rate (a$^{-1}$)")
-plt.savefig("tau-rate.png")
+plt.ylabel(r"ratio")
+plt.savefig("tau-ratio.png")
 
-plot_histogram(theta, weight, generated)
+plot_histogram(theta, weight[:, 0], generated)
 plt.xlabel(r"zenith, $\theta_\tau$ (deg)")
 plt.ylabel(r"rate (deg$^{-1}$ a$^{-1}$)")
 plt.savefig("tau-zenith.png")
 
-plot_histogram(phi, weight, generated)
+plot_histogram(phi, weight[:, 0], generated)
 plt.axis((-200., 200., 0., 2E-02))
 plt.xticks(numpy.linspace(-180., 180., 5))
 plt.yticks(numpy.linspace(0., 2E-02, 5))
@@ -115,8 +132,10 @@ plt.xlabel(r"azimuth, $\phi_\tau$ (deg)")
 plt.ylabel(r"rate (deg$^{-1}$ a$^{-1}$)")
 plt.savefig("tau-azimuth.png")
 
-plot_histogram(altitude, weight, generated, plot=plt.semilogy)
-plt.axis((0., 5., 1E-04, 1E+02))
+plot_histogram(altitude, weight[:, 0], generated, plot=plt.semilogy)
+plot_histogram(altitude, weight[:, 1], generated, plot=plt.semilogy, new=False,
+               clr="r")
+plt.axis((0., 3., 1E-03, 1E+02))
 plt.xlabel(r"decay altitude, h$_\tau$ (km)")
 plt.ylabel(r"rate (km$^{-1}$ a$^{-1}$)")
 plt.savefig("tau-altitude.png")
